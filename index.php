3d274e5e1d7d25a0dc6951124c77b79bb2fa6daf
Rostazore <11044833+Rostazore@users.noreply.github.com>
2017-11-28 10:28:13 +0100

    Dépot initial de mon thème

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Ici, je suis sur le fichier par défaut pour tout thème WP.
+ * Il est l'un des 2 OBLIGATOIRES (l'autre étant style.css).
+ * Il est utilisé pour afficher une page si aucun autre fichier
+ * n'est disponible.
+ */
+?>
+        
